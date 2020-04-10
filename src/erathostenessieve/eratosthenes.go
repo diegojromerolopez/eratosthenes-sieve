86cf846d3e00339ba9fc8dc9493a60c5c753ab79
Diego J <diegojromerolopez@gmail.com>
2020-04-11 01:01:57 +0200

    Fix algorithm

--- src/erathostenessieve/eratosthenes.go
+++ src/erathostenessieve/eratosthenes.go
@@ -1,13 +1,12 @@
 package eratosthenessieve
 
 import (
-	"fmt"
 	"math/big"
 )
 
 // SieveOfErathostenes : a big int version of the sieve of Erathostenes
 type SieveOfErathostenes struct {
-	limit          *big.Int
+	limit       *big.Int
 	primeStorer func(*big.Int)
 }
 
@@ -23,12 +22,11 @@ func (sieve SieveOfErathostenes) Run() {
 	one := big.NewInt(int64(1))
 	two := big.NewInt(int64(2))
 	isPrime := make(map[string]bool)
-	for i := two; i.Cmp(sieve.limit) < 0; i = new(big.Int).Add(i, one) {
-		fmt.Printf("Number %s\n", i.String())
+	for i := two; i.Cmp(sieve.limit) <= 0; i = new(big.Int).Add(i, one) {
 		iIsPrime, iPrimalityIsComputed := isPrime[i.String()]
 		if iIsPrime || !iPrimalityIsComputed {
 			jStart := new(big.Int).Exp(i, two, nil)
-			for j := jStart; j.Cmp(sieve.limit) < 0; j = new(big.Int).Add(j, i) {
+			for j := jStart; j.Cmp(sieve.limit) <= 0; j = new(big.Int).Add(j, i) {
 				isPrime[j.String()] = false
 			}
 			sieve.primeStorer(i)
--- src/erathostenessieve/eratosthenes_test.go
+++ src/erathostenessieve/eratosthenes_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestSieveOfErathostenes(t *testing.T) {
-	n := big.NewInt(33)
+	n := big.NewInt(31)
 
 	var primes []*big.Int
 	primeStorer := func(prime *big.Int) {
@@ -17,18 +17,23 @@ func TestSieveOfErathostenes(t *testing.T) {
 	sieve.Run()
 
 	expectedPrimes := []int64{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31}
-	badPrimesCount := 0
-	for i := 0; i < len(primes); i++ {
-		expectedPrimeI := big.NewInt(int64(expectedPrimes[i]))
-		if expectedPrimeI.Cmp(primes[i]) != 0 {
-			t.Errorf("Prime %d not found. Found %s instead",
-				expectedPrimes[i], primes[i].String())
-			badPrimesCount++
-		}
-	}
-	if badPrimesCount == 0 {
-		t.Logf("Prime OK")
+	if len(expectedPrimes) != len(primes) {
+		t.Errorf("Prime count failed on: %d primes expected, %d found.",
+			len(expectedPrimes), len(primes))
 	} else {
-		t.Errorf("Prime count failed on: %d primes", badPrimesCount)
+		badPrimesCount := 0
+		for i := 0; i < len(primes); i++ {
+			expectedPrimeI := big.NewInt(int64(expectedPrimes[i]))
+			if expectedPrimeI.Cmp(primes[i]) != 0 {
+				t.Errorf("Prime %d not found. Found %s instead.",
+					expectedPrimes[i], primes[i].String())
+				badPrimesCount++
+			}
+		}
+		if badPrimesCount == 0 {
+			t.Logf("Prime OK")
+		} else {
+			t.Errorf("Prime count failed on: %d primes", badPrimesCount)
+		}
 	}
 }
